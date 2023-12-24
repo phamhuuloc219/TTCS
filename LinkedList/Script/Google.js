a4a1c6507beffa97a16891a3a39028b800e5d1a1
phamhuuloc219 <94950295+phamhuuloc219@users.noreply.github.com>
2023-12-24 20:04:46 +0700

    linked-list

---
+++ LinkedList/Script/Google.js
@@ -0,0 +1,18 @@
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'script',
+        success: callback,
+        async: true
+    });
+}
+
+$.loadScript('https://www.googletagmanager.com/gtag/js?id=UA-97194730-8', function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-97194730-8');
+});
